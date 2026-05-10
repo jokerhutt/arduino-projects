62083023183fcf8db634055ca10ab6b6680d973e
Dave <davidglogowski2003@gmail.com>
2026-05-10 20:36:34 +0200

    clean blink & button a bit

--- blink/main.c
+++ blink/main.c
@@ -3,17 +3,17 @@
 
 int main(void) {
 
-    // Pin 5 in port B is output
-    DDRB |= (1 << PB5);
-    DDRD &= ~(1 << PD2);
+  // Pin 5 in port B is output
+  DDRB |= (1 << PB5);
+  DDRD &= ~(1 << PD2);
 
-    while (1) {
-        // Turn on Port B Pin 5
-        PORTB |= (1 << PB5);
-        _delay_ms(500);
+  while (1) {
+    // Turn on Port B Pin 5
+    PORTB |= (1 << PB5);
+    _delay_ms(500);
 
-        // Turn off Port B Pin 5
-        PORTB &= ~(1 << PB5);
-        _delay_ms(500);
-    }
+    // Turn off Port B Pin 5
+    PORTB &= ~(1 << PB5);
+    _delay_ms(500);
+  }
 }
--- button/main.c
+++ button/main.c
@@ -1,23 +1,39 @@
 #define F_CPU 16000000UL
 #include <avr/io.h>
+#include <util/delay.h>
 
 int main(void) {
 
-  // LED (PB5 / D13) = output
-  DDRB |= (1 << PB5);
+    // LED (PB5 / D13) = OUTPUT
+    DDRB |= (1 << PB5);
 
-  // Button (PD2 / D2) = input
-  DDRD &= ~(1 << PD2);
+    // Button (PD2 / D2) INPUT
+    DDRD &= ~(1 << PD2);
 
-  // Enable internal pull-up on D2
-  PORTD |= (1 << PD2);
+    // Pull up on D2
+    PORTD |= (1 << PD2);
 
-  while (1) {
-    // If button pressed (reads LOW)
-    if (!(PIND & (1 << PD2))) {
-      PORTB |= (1 << PB5); // LED ON
-    } else {
-      PORTB &= ~(1 << PB5); // LED OFF
+    uint8_t ledState = 0;
+    uint8_t lastButton = 0;
+
+    while (1) {
+
+        uint8_t isPressed = !(PIND & (1 << PD2));
+
+        // if pressed
+        if (!lastButton && isPressed) {
+            ledState = !ledState;
+
+            // If turning on
+            if (ledState == 1) {
+                PORTB |= (1 << PB5); // LED ON
+            } else {
+                PORTB &= ~(1 << PB5); // LED OFF
+            }
+
+            _delay_ms(200);
+        }
+
+        lastButton = isPressed;
     }
-  }
 }
